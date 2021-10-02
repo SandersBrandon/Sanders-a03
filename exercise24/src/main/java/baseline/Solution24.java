f5de89161a572176ecf63bcb1a937cd834a177a7
SandersBrandon <brandosan1@icloud.com>
2021-10-02 00:00:21 -0400

    adding files

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -3,12 +3,32 @@
  *  Copyright 2021 Brandon Sanders
  */
 
+import java.util.Scanner;
 
 package baseline;
 
 public class Solution24 {
     /* Firstly print then get user input */
-    /* Create a boolean method isAnagram */
+    public static void main(String[] args) {
+
+    }
+    void GetInput(){
+        private static final Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two strings and I'll tell you if they are anagrams: “\\r\\n” Enter the first string : ");
+        String firstInput = in.nextLine();
+        System.out.println("Enter the second string: ");
+        String secondInput = in.nextLine();
+    }
+    /* Create a boolean function isAnagram */
+   void isInputAnagram{
+       boolean t1 ;
+       boolean f1 ;
+       t1 = true;
+       f1 = false;
+       
+
+    }
     /*Compare the two strings and return true or false if theyre anagrams*/
     /*Print out the input and if they are anagrams*/
 }
