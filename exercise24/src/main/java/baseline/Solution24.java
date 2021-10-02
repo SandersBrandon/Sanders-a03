26068f35491a006cab847e8d5986f6320c36c413
SandersBrandon <brandosan1@icloud.com>
2021-10-01 23:33:31 -0400

    adding pseudocode

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brandon Sanders
+ */
+
+
+package baseline;
+
+public class Solution24 {
+    /* Firstly print then get user input */
+    /* Create a boolean method isAnagram */
+    /*Compare the two strings and return true or false if theyre anagrams*/
+    /*Print out the input and if they are anagrams*/
+}
