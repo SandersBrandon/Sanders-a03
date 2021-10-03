55b1fd0e721a38bec874dd3daf7fccad14b8dcc2
SandersBrandon <brandosan1@icloud.com>
2021-10-02 22:55:56 -0400

    adding completed exercise 24

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -3,32 +3,55 @@
  *  Copyright 2021 Brandon Sanders
  */
 
-import java.util.Scanner;
-
 package baseline;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
+/* Firstly print then get user input */
+/* Create a boolean function isAnagram */
+/*Compare the two strings and return true or false if theyre anagrams*/
+/*Print out the input and if they are anagrams*/
+
+
 public class Solution24 {
-    /* Firstly print then get user input */
-    public static void main(String[] args) {
+
+    static void isInputAnagram(String firstInput, String secondInput){
+       boolean  status = true;
+       if (firstInput.length() != secondInput.length()){
+           status= false;
+
+       }
+       else {
+           char[] firstInputArray = firstInput.toCharArray();
+           char[] secondInputArray = secondInput.toCharArray();
+           Arrays.sort(firstInputArray);
+           Arrays.sort(secondInputArray);
+           status =  Arrays.equals(firstInputArray , secondInputArray);
+
+       }
+       if (status){
+           System.out.println("'" + firstInput + "'" + " and " + "'" + secondInput + "'" + " are anagrams.");
+       }
+       else {
+           System.out.println("'" + firstInput + "'" + " and " + "'" + secondInput + "'" + " are not anagrams.");
+       }
+
+
 
     }
-    void GetInput(){
-        private static final Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter two strings and I'll tell you if they are anagrams: “\\r\\n” Enter the first string : ");
+
+    public static void main(String[] args) {
+        final Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two strings and I'll tell you if they are anagrams:  \r\n  Enter the first string : ");
         String firstInput = in.nextLine();
         System.out.println("Enter the second string: ");
         String secondInput = in.nextLine();
-    }
-    /* Create a boolean function isAnagram */
-   void isInputAnagram{
-       boolean t1 ;
-       boolean f1 ;
-       t1 = true;
-       f1 = false;
+
+        isInputAnagram(firstInput, secondInput);
 
 
     }
-    /*Compare the two strings and return true or false if theyre anagrams*/
-    /*Print out the input and if they are anagrams*/
 }
