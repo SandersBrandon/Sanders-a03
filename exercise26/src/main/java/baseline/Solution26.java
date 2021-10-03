5516c6bd7c9d2afe349765a4726c08e8b6a0f9fe
SandersBrandon <brandosan1@icloud.com>
2021-10-03 16:42:14 -0400

    adding exercise 26 pseudocode

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brandon Sanders
+ */
+
+package baseline;
+
+/* Get user input turn it into necessary data
+*Create a decimal from the APR percentage
+* Create a class Payment Calculator with a method calculateMonthsUntilPaidOff and find number of months
+*Round up fractions to nearest cent
+*Output necessary information
+*/
+
+public class Solution26 {
+    public static void main(String[] args) {
+
+    }
+}
