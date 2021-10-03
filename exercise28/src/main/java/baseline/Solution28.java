949724a5e067469c826d1d9339d5ce462c5b0624
SandersBrandon <brandosan1@icloud.com>
2021-10-03 19:56:01 -0400

    adding pseudocode for exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brandon Sanders
+ */
+package baseline;
+
+import java.util.Scanner;
+
+/*Create loop to ask for user input 5 times
+*Store the inputs into an array
+*Sum up the array
+* Output the added sum of the array
+*/
+
+public class Solution28 {
+    public static void main(String[] args) {
+
+    }
+}
