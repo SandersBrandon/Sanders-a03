1c25e85d36ef76302511651de9dfe00c2c19fb7c
SandersBrandon <brandosan1@icloud.com>
2021-10-03 22:27:02 -0400

    adding exercise 30 pseudocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brandon Sanders
+ */
+package baseline;
+
+import java.util.Scanner;
+
+/*Creating multiplication table of fixed size and fixed numbers
+Create nested loops to keep track of the row
+* Create nested loop to keep track of the number we are multiplying
+* Format output so it looks good
+*/
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+
+    }
+}
