962f2d060d0c8b8636d6ff83d6ed1c7750cd0361
SandersBrandon <brandosan1@icloud.com>
2021-10-03 20:50:28 -0400

    addin exercise 29 pseudocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Brandon Sanders
+ */
+package baseline;
+
+import java.util.Scanner;
+
+
+/* Ask for user input in a loop
+*Create loop conditions where input is valid
+* Throw out bad inputs
+*Ask again for valid input
+* Accept valid input calculate required information
+* Output answer 
+*/
+public class Solution29 {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
